e66dc9567e3086fa7c3519bcae76b3d2f7dacb62
MostafaRashed11001 <mostafarashed11001@gmail.com>
2023-08-11 12:56:03 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program writes the lowercase English alphabet
+ * characters to the standard output (stdout).
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declare and initialize the character to be written */
+	char ch = '1';
+	/* Write the lowercase English alphabet characters to stdout */
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	/* Write a newline character to separate output */
+	putchar('\n');
+	return (0);
+}
