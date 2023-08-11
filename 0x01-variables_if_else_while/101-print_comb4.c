ee06419d5c7b165e801ef577187703f945be6bc4
MostafaRashed11001 <mostafarashed11001@gmail.com>
2023-08-11 13:52:32 -0700

    numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program writes the lowercase English alphabet
+ * characters to the standard output (stdout).
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declare and initialize the character to be written */
+	int ch = 48, num = 48, last = 48;
+	/* Write the lowercase English alphabet characters to stdout */
+	for (num = 48; num <= 57; num++)
+	{
+		for (ch = num + 1; ch <= 57; ch++)
+		{
+			for (last = ch + 1; last <= 57; last++)
+			{
+				if (ch != 50)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				putchar(num);
+				putchar(ch);
+				putchar(last);
+			}
+		}
+	}
+	/* Write a newline character to separate output */
+	putchar('\n');
+	return (0);
+}
