797f80bd341316d8535b295f44bb9032591db32f
MostafaRashed11001 <mostafarashed11001@gmail.com>
2023-08-11 12:36:49 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program writes the lowercase English alphabet
+ * characters to the standard output (stdout).
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declare and initialize the character to be written */
+	char ch = '1';
+	/* Write the lowercase English alphabet characters to stdout */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	/* Write a newline character to separate output */
+	putchar('\n');
+	return (0);
+}
