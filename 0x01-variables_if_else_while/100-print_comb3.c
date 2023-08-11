f111ec47385e9cf77472782a7bf69409ee6686d1
MostafaRashed11001 <mostafarashed11001@gmail.com>
2023-08-11 13:43:23 -0700

    numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program writes the lowercase English alphabet
+ * characters to the standard output (stdout).
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declare and initialize the character to be written */
+	int ch = 48, num = 48;
+	/* Write the lowercase English alphabet characters to stdout */
+	for (num = 48; num <= 57; num++)
+	{
+		for (ch = num + 1; ch <= 57; ch++)
+		{
+			if (ch != 48)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			putchar(num);
+			putchar(ch);
+		}
+	}
+	/* Write a newline character to separate output */
+	putchar('\n');
+	return (0);
+}
