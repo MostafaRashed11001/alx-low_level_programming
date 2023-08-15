6807ccd220066fadd5d43bac4b4d02b67698c7e8
MostafaRashed11001 <mostafarashed11001@gmail.com>
2023-08-15 14:25:16 -0700

    hi

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -46,5 +46,20 @@ int _islower(int c);
  * It returns 1 if the character is an alphabetic letter, and 0 otherwise.
  */
 int _isalpha(int c);
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ *
+ * Return: 1 and prints + if n is greater than zero,
+ *         0 and prints 0 if n is zero,
+ *        -1 and prints - if n is less than zero
+ *
+ * Description:
+ * This function prints the sign of the given number.
+ * It returns 1 and prints '+' if the number is greater than zero,
+ * returns 0 and prints '0' if the number is zero,
+ * and returns -1 and prints '-' if the number is less than zero.
+ */
+int print_sign(int n);
 
 #endif /* MAIN_H */
