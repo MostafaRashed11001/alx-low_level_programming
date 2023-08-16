28bb7d3dd5006ee4ae2f147ea1be7d048d2d5d48
MostafaRashed11001 <mostafarashed11001@gmail.com>
2023-08-15 17:09:43 -0700

    hi

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-	unsigned long int fib1 = 1, fib2 = 2, next_fib, i;
+	unsigned long long fib1 = 1, fib2 = 2, next_fib, i;
 	/* Print the first two Fibonacci numbers */
-	printf("%lu, %lu", fib1, fib2);
+	printf("%llu, %llu", fib1, fib2);
 	for (i = 2; i < 98; i++)
 	{
 		next_fib = fib1 + fib2;
-		printf(", %lu", next_fib);
+		printf(", %llu", next_fib);
 		fib1 = fib2;
 		fib2 = next_fib;
 	}
