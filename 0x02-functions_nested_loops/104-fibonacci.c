b22bf8ae8eb84fc89fec00a4d89d8a58a4c5b7b1
MostafaRashed11001 <mostafarashed11001@gmail.com>
2023-08-15 18:32:53 -0700

    hi

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,60 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * numLength - Calculates the number of digits in a number.
+ * @num: The number whose length is to be calculated.
  *
- * Description: Prints the first 98 Fibonacci numbers.
+ * Return: The number of digits in the given number.
+ */
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
+/**
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long int fib1 = 1, fib2 = 2, next_fib, i;
-	/* Print the first two Fibonacci numbers */
-	printf("%lu, %lu", fib1, fib2);
-	for (i = 2; i < 98; i++)
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
+	for (count = 1; count <= 98; count++)
 	{
-		next_fib = fib1 + fib2;
-		printf(", %lu", next_fib);
-		fib1 = fib2;
-		fib2 = next_fib;
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			initial0s--;
+		}
+
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
