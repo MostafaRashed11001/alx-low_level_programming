1edcaa64788ba2da94f7fbcab0999e3ca57d2acf
MostafaRashed11001 <mostafarashed11001@gmail.com>
2023-08-17 16:21:11 -0700

    function and loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-/* Include the standard I/O library */
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints numbers from 1 to 100, replacing multiples of 3 with
+ * "Fizz", multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
-	/* Declare the loop counter */
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -26,8 +34,11 @@ int main(void)
 			printf("%d ", i);
 			/* Print the number itself */
 		}
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	/* Print a newline character to move to the next line */
 	return (0);
 	/* Return 0 to indicate successful execution */
